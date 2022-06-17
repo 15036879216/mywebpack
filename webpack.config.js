3b1efc7e1766a693ec5310a48a87c7bb1100895b
ZMW <2412969135@qq.com>
2022-06-17 20:17:29 +0800

    更新自动生成html功能

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const {join} =require('path')
-
+// 引入自动生成 html 的插件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports={
+    //development 设为开发者模式
+    mode: 'development',
     // 要求从src/main.js  打包到 lib/index.js
     //入口
     entry:'./src/main.js',
@@ -10,5 +13,16 @@ module.exports={
         filename:'index.js',
         //清除之前的打包
         clean:true
+    },
+    //自动生成html
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:join(__dirname,'public/index.html')
+        })
+    ],
+    devServer:{
+        open:true
     }
 }
+
+
